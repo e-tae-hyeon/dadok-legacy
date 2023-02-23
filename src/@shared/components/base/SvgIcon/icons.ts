b537749753a7fca2cafcd9f457efbf6bd5172f67
LTH <johtta888@naver.com>
2023-02-24 00:07:27 +0900

    Feat: 추가 svg icon

---
+++ src/@shared/components/base/SvgIcon/icons.ts
@@ -0,0 +1,3 @@
+export { default as bookClosed } from "assets/icons/book-closed.svg";
+export { default as edit } from "assets/icons/edit.svg";
+export { default as home } from "assets/icons/home.svg";
